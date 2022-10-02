12fce8549826e894b3022b36efd57dd3bf29c533
Ruediger zu Dohna <snackbox@sinntr.eu>
2022-10-02 12:26:32 +0200

    run IT with Jee-Testcontainers

--- src/test/java/test/HelloIT.java
+++ src/test/java/test/HelloIT.java
@@ -1,16 +1,24 @@
 package test;
 
+import com.github.t1.testcontainers.jee.JeeContainer;
+import com.github.t1.testcontainers.jee.WildflyContainer;
 import org.junit.jupiter.api.Test;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse.BodyHandlers;
 
 import static org.assertj.core.api.BDDAssertions.then;
 
+@Testcontainers
 class HelloIT {
     private static final HttpClient HTTP = HttpClient.newHttpClient();
+
+    @Container @SuppressWarnings("resource")
+    static JeeContainer CONTAINER = WildflyContainer.create("rdohna/wildfly", "26.1-jdk17")
+        .withDeployment("target/ROOT.war");
 
     @Test
     void shouldGetCachedValue() throws Exception {
@@ -24,7 +32,7 @@ class HelloIT {
 
     private static String get() throws Exception {
         var get = HttpRequest.newBuilder()
-            .uri(URI.create("http://localhost:8080/hi"))
+            .uri(CONTAINER.baseUri().resolve("hi"))
             .header("Accept", "application/json")
             .GET()
             .build();
